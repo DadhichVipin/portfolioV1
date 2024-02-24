22e83bdb3f4c6e8e32db8038d523ff0dbd170a8b
Vipin <vd02172000@gmail.com>
2024-02-24 12:14:40 +0530

    update: add static files , reqirments and psql

--- core/settings.py
+++ core/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': '61-D*3-F42E13d5gDF5C4g14AGEgGABB',
-#         'HOST': 'monorail.proxy.rlwy.net',
-#         'PORT': '50468',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '61-D*3-F42E13d5gDF5C4g14AGEgGABB',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '50468',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
