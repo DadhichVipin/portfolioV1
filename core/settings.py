022b4b111eba836f071813ff028bf7aab77bf4c4
Vipin <vd02172000@gmail.com>
2024-02-25 12:57:47 +0530

    update for vercel

--- core/settings.py
+++ core/settings.py
@@ -128,7 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, images)
 # Static files (CSS, JavaScript, images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
